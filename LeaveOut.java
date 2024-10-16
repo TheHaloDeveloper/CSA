56d6b338423f996fbbdf6551648b8986441b92b7
Halo <68120801+TheHaloDeveloper@users.noreply.github.com>
2024-10-16 15:33:31 -0700

    leave out lab

---
+++ LeaveOut.java
@@ -0,0 +1,5 @@
+public class LeaveOut {
+    public static String go(String a, int i) {
+        return a.substring(0, i) + a.substring(i + 1);
+    }
+}
--- SwitchFirst.java
+++
@@ -1,5 +0,0 @@
-public class SwitchFirst {
-    public static String go( String a, String b ){
-        return b.charAt(0) + a.substring(1);
-    }
-}
