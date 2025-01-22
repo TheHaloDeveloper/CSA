0fb4012668435f91bedb370eec23c09cea4bfede
Halo <68120801+TheHaloDeveloper@users.noreply.github.com>
2025-01-21 23:02:24 -0800

    part 3

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int number = 75;
+
+        if (number > 50 && number % 5 == 0) {
+            System.out.println("The number is greater than 50 and divisible by 5.");
+        } else {
+            System.out.println("The number is not greater than 50 and divisible by 5.");
+        }
+    }
+}
--- PersonRunner.java
+++
@@ -1,8 +0,0 @@
-public class PersonRunner {
-    public static void main( String[] args ) {
-        Person p1 = new Person("John Doe", 25);
-        p1.displayInfo();
-        Person p2 = new Person("Jane Smith", 30);
-        p2.displayInfo();
-    }
-}
