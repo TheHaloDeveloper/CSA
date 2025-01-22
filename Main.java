b9e00eba92cc3d58281b9505b13f8d8f8724232b
Halo <68120801+TheHaloDeveloper@users.noreply.github.com>
2025-01-21 23:03:23 -0800

    part 4

--- Main.java
+++ Main.java
@@ -1,11 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        int number = 75;
+        for (int i = 2; i <= 50; i += 2) {
+            System.out.print(i + " ");
+        }
 
-        if (number > 50 && number % 5 == 0) {
-            System.out.println("The number is greater than 50 and divisible by 5.");
-        } else {
-            System.out.println("The number is not greater than 50 and divisible by 5.");
+        int i = 2;
+        while (i <= 50) {
+            System.out.print(i + " ");
+            i += 2;
         }
     }
 }
