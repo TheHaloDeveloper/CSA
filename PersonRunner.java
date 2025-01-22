af615a9288ad23483956c60d44d011f5b0529bbf
Halo <68120801+TheHaloDeveloper@users.noreply.github.com>
2025-01-21 22:58:31 -0800

    person (part 2)

---
+++ PersonRunner.java
@@ -0,0 +1,8 @@
+public class PersonRunner {
+    public static void main( String[] args ) {
+        Person p1 = new Person("John Doe", 25);
+        p1.displayInfo();
+        Person p2 = new Person("Jane Smith", 30);
+        p2.displayInfo();
+    }
+}
