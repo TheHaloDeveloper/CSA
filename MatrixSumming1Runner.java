1bab4b7772dddebaa976b8aa3095dc44a116e3ba
Halo <68120801+TheHaloDeveloper@users.noreply.github.com>
2025-04-16 20:24:19 -0700

    matrix sum

---
+++ MatrixSumming1Runner.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import static java.lang.System.*;
+
+public class MatrixSumming1Runner
+{
+	public static void main( String args[] ) throws Exception
+	{
+		System.out.print("The sum is :: ");
+        System.out.println(MatrixSumming1.sum());
+	}
+}
+
+
